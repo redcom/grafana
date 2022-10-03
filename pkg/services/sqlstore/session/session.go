3988ffe0743e45298ffee3b0226296fee043fcad
Ying WANG <ying.wang@grafana.com>
2022-10-03 10:50:22 +0200

    add sqlxTransaction

--- pkg/services/sqlstore/session/session.go
+++ pkg/services/sqlstore/session/session.go
@@ -72,6 +72,14 @@ type SessionTx struct {
 	sqlxtx *sqlx.Tx
 }
 
+func (gtx *SessionTx) Commit() error {
+	return gtx.sqlxtx.Commit()
+}
+
+func (gtx *SessionTx) Rollback() error {
+	return gtx.sqlxtx.Rollback()
+}
+
 func (gtx *SessionTx) NamedExec(ctx context.Context, query string, arg interface{}) (sql.Result, error) {
 	return gtx.sqlxtx.NamedExecContext(ctx, gtx.sqlxtx.Rebind(query), arg)
 }
